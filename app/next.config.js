f6eea9ffec4837b6b2af0078f4d471e2120c6421
Berzan <berzan@tutanota.com>
2023-10-10 19:26:21 +0000

    update: config for GitHub Pages

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
+  basePath: process.env.NODE_ENV === "production" ? "/nft-pawn-shop" : undefined
 }
 
 module.exports = nextConfig

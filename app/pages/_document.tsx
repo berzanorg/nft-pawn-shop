7acec871d87fe24f0473f42653486d152cee8a35
Berzan <berzan@tutanota.com>
2023-10-10 19:15:17 +0000

    use: `Onest` font

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -1,13 +1,18 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { Onest } from 'next/font/google'
+
+
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Onest:wght@400;500;600;700&display=swap" rel="stylesheet" />
+      </Head>
+      <body className="font-['Onest',sans-serif]">
         <Main />
         <NextScript />
       </body>
-    </Html>
+    </Html >
   )
 }

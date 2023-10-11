34733895bf7eb69b15dc824f0cb91f07beea31c2
Berzan <berzan@tutanota.com>
2023-10-11 09:16:09 +0000

    remove: template

--- app/components/Copyright.js
+++
@@ -1,8 +0,0 @@
-const Copyright = () => {
-    return(
-        <>
-            <p className="uk-text-muted">© 2023 NFT Pawn Shop. All rights reserved.</p>
-        </>
-    )
-}
-export default Copyright

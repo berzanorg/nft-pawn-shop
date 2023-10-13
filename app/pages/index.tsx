6ce8ac1eace4eb56ac8ffe5feff8cb6bbf6e0cd9
Berzan <berzan@tutanota.com>
2023-10-13 20:07:37 +0000

    update: `index` page

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,12 +1,20 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { PopupRequestDemoAssets } from "@/components/PopupRequestDemoAssets";
+import { Main } from "@/components/Main";
+import { NFTCard } from "@/components/NFTCard";
+import { useDapp } from "@/contexts/DappProvider";
 
 export default function Home() {
+
+  const { orders } = useDapp()
+
   return (
-    <main className=''>
-      hello world
-    </main>
+    <Main>
+      <div className="flex flex-col gap-3">
+        <h1 className="text-3xl font-bold md:text-4xl">Listed NFTs</h1>
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-4 gap-y-6">
+          {orders?.map((order) => <NFTCard key={`o${order.account}${order.index}`} order={order} />)}
+        </div>
+      </div>
+    </Main>
   )
 }

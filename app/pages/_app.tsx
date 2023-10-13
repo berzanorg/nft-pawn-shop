0c0557307f990e3db559ca3f36a9c31fbf7d8b8f
Berzan <berzan@tutanota.com>
2023-10-13 20:07:03 +0000

    add: providers

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,6 +1,45 @@
+import { Bottombar } from '@/components/Bottombar'
+import { Navbar } from '@/components/Navbar'
+import { PopupRequestDemoAssets } from '@/components/PopupRequestDemoAssets'
+import { DappProvider } from '@/contexts/DappProvider'
+import { PopupProvider } from '@/contexts/PopupProvider'
 import '@/styles/globals.css'
+import '@/styles/wallet-adapter.css'
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { clusterApiUrl } from '@solana/web3.js'
 import type { AppProps } from 'next/app'
+import { useMemo } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+    const endpoint = useMemo(() => clusterApiUrl(WalletAdapterNetwork.Devnet), [])
+
+    const wallets = useMemo(
+        () => [
+            new PhantomWalletAdapter(),
+            new SolflareWalletAdapter(),
+        ],
+        []
+    )
+
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>
+                    <DappProvider>
+                        <PopupProvider>
+                            <Navbar />
+                            <PopupRequestDemoAssets />
+                            <Component {...pageProps} />
+                            <Bottombar />
+                        </PopupProvider>
+                    </DappProvider>
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    )
+
 }

044eb38efa9ddeee192034697e1e06d619ca8718
Berzan <berzan@tutanota.com>
2023-11-22 12:13:11 +0000

    update: `CustomError` variants

--- programs/nft-pawn-shop/src/errors.rs
+++ programs/nft-pawn-shop/src/errors.rs
@@ -2,27 +2,6 @@ use anchor_lang::prelude::*;
 
 #[error_code]
 pub enum CustomError {
-    #[msg("Debt payment deadline is over.")]
-    DebtPaymentDeadlineIsOver,
-
-    #[msg("Debt payment deadline is not over.")]
-    DebtPaymentDeadlineIsValid,
-
-    #[msg("You do not have enough demo tokens.")]
-    InsufficientDemoTokens,
-
-    #[msg("You have no demo NFT.")]
-    NoDemoNFT,
-
-    #[msg("No debt found at specified index.")]
-    NoDebtFound,
-
-    #[msg("No order found at specified index.")]
-    NoOrderFound,
-
-    #[msg("Specifed lender is not the expected lender.")]
-    WrongLender,
-
-    #[msg("You do not have access to complete this operation.")]
-    UnauthorizedAccess,
+    #[msg("You don't have enough SOL balance.")]
+    NotEnoughBalance,
 }

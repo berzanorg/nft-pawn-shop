73add40b986ce7a6dacdc3759b855d50a213910a
Berzan <berzan@tutanota.com>
2023-11-28 20:04:08 +0000

    use: new providers

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,21 +1,19 @@
 import { Bottombar } from '@/components/Bottombar'
 import { Navbar } from '@/components/Navbar'
-import { PopupRequestDemoAssets } from '@/components/PopupRequestDemoAssets'
-import { DappProvider } from '@/contexts/DappProvider'
+import { NFTsProvider } from '@/contexts/NFTsProvider'
+import { PawnShopProvider } from '@/contexts/PawnShopProvider'
 import { PopupProvider } from '@/contexts/PopupProvider'
 import '@/styles/globals.css'
 import '@/styles/wallet-adapter.css'
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
 import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
-import { clusterApiUrl } from '@solana/web3.js'
 import type { AppProps } from 'next/app'
 import { useMemo } from 'react'
 
-export default function App({ Component, pageProps }: AppProps) {
+const endpoint = 'https://devnet.helius-rpc.com/?api-key=db25ae76-7277-45ce-b21a-5be1a61f2f04'
 
-    const endpoint = useMemo(() => clusterApiUrl(WalletAdapterNetwork.Devnet), [])
+export default function App({ Component, pageProps }: AppProps) {
 
     const wallets = useMemo(
         () => [
@@ -29,14 +27,15 @@ export default function App({ Component, pageProps }: AppProps) {
         <ConnectionProvider endpoint={endpoint}>
             <WalletProvider wallets={wallets} autoConnect>
                 <WalletModalProvider>
-                    <DappProvider>
-                        <PopupProvider>
-                            <Navbar />
-                            <PopupRequestDemoAssets />
-                            <Component {...pageProps} />
-                            <Bottombar />
-                        </PopupProvider>
-                    </DappProvider>
+                        <NFTsProvider>
+                    <PawnShopProvider>
+                            <PopupProvider>
+                                <Navbar />
+                                <Component {...pageProps} />
+                                <Bottombar />
+                            </PopupProvider>
+                    </PawnShopProvider>
+                        </NFTsProvider>
                 </WalletModalProvider>
             </WalletProvider>
         </ConnectionProvider>

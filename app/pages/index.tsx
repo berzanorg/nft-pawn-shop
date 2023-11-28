08127f888bd92379dc808dadb09e56ce37c3932c
Berzan <berzan@tutanota.com>
2023-11-28 20:04:14 +0000

    update: content

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,18 +1,18 @@
-import { PopupRequestDemoAssets } from "@/components/PopupRequestDemoAssets";
 import { Main } from "@/components/Main";
 import { NFTCard } from "@/components/NFTCard";
-import { useDapp } from "@/contexts/DappProvider";
-
+import { usePawnShop } from "@/contexts/PawnShopProvider";
 export default function Home() {
 
-  const { orders } = useDapp()
+  const { orders } = usePawnShop()
 
   return (
     <Main>
       <div className="flex flex-col gap-3">
-        <h1 className="text-3xl font-bold md:text-4xl">Listed NFTs</h1>
+        <h1 className="text-3xl font-bold md:text-4xl">NFT Pawn Shop</h1>
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-4 gap-y-6">
-          {orders?.map((order) => <NFTCard key={`o${order.account}${order.index}`} order={order} />)}
+          {orders.length > 0 ? (
+            orders.map((order) => <NFTCard key={order.pda.toString()} kind="order" data={order} />)
+          ) : <p className="font-medium text-white/60">No NFTs to be pawned.</p>}
         </div>
       </div>
     </Main>
--- app/pages/user-profile.tsx
+++ app/pages/user-profile.tsx
@@ -1,43 +1,53 @@
 import { Main } from "@/components/Main";
 import { NFTCard } from "@/components/NFTCard";
-import { useDapp } from "@/contexts/DappProvider";
+import { useNFTs } from "@/contexts/NFTsProvider";
+import { usePawnShop } from "@/contexts/PawnShopProvider";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { useMemo } from "react";
 
 export default function Profile() {
     const { publicKey } = useWallet()
-    const { demoNFTsCount, demoTokensBalance, userLends, userDebts, userOrders } = useDapp()
-
-    const availableNFTs = useMemo(() => [...Array(demoNFTsCount).keys()], [demoNFTsCount])
+    const { userOrders, userPawnedNFTs, userLends } = usePawnShop()
+    const { nfts } = useNFTs()
 
     return (
         <Main>
             <div className="flex flex-col gap-10">
-                {availableNFTs.length > 0 && (
-                    <div className="flex flex-col gap-3">
-                        <h2 className="text-3xl font-bold md:text-4xl">Your Available NFTs</h2>
+                <div className="flex flex-col gap-3">
+                    <h2 className="text-3xl font-bold md:text-4xl">Your Available NFTs</h2>
+                    {nfts.length > 0 ? (
                         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-3 gap-y-3">
-                            {availableNFTs.map((i) => <NFTCard key={`p${publicKey}${i}`} />)}
+                            {nfts.map((nft) => <NFTCard key={nft.mint} kind="justNFT" data={nft} />)}
                         </div>
-                    </div>
-                )}
-                {(userOrders?.at(0) || userDebts?.at(0)) && (
-                    <div className="flex flex-col gap-3">
-                        <h2 className="text-3xl font-bold md:text-4xl">Your Unavailable NFTs</h2>
+                    ) : <p className="font-medium text-white/60">You don't have any NFTs available.</p>}
+                </div>
+                <div className="flex flex-col gap-3">
+                    <h2 className="text-3xl font-bold md:text-4xl">Your Unavailable NFTs</h2>
+                    {userOrders.length > 0 ? (
                         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-3 gap-y-3">
-                            {userOrders?.map((order, i) => <NFTCard key={`o${publicKey}${i}`} order={order} />)}
-                            {userDebts?.map((debt, i) => <NFTCard key={`d${publicKey}${i}`} debt={debt} />)}
+                            {userOrders.map((order) => <NFTCard key={order.pda.toString()} kind="userOrder" data={order} />)}
                         </div>
-                    </div>
-                )}
-                {userLends?.at(0) && (
-                    <div className="flex flex-col gap-3">
-                        <h2 className="text-3xl font-bold md:text-4xl">Debtors' NFTs</h2>
+                    ) : <p className="font-medium text-white/60">You don't have any NFTs unavailable .</p>}
+                </div>
+                <div className="flex flex-col gap-3">
+                    <h2 className="text-3xl font-bold md:text-4xl">Your Pawned NFTs</h2>
+                    {userPawnedNFTs.length > 0 ? (
                         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-3 gap-y-3">
-                            {userLends?.map((lend, i) => <NFTCard key={`l${publicKey}${i}`} lend={lend} />)}
+                            {userPawnedNFTs.map((pawnedNFT, i) => <NFTCard key={pawnedNFT.pda.toString()} kind="userPawnedNFT" data={pawnedNFT} />)}
                         </div>
-                    </div>
-                )}
+
+                    ) : <p className="font-medium text-white/60">You don't have any NFTs pawned.</p>}
+                </div>
+
+                <div className="flex flex-col gap-3">
+                    <h2 className="text-3xl font-bold md:text-4xl">NFTs You Lended For</h2>
+                    {userLends.length > 0 ? (
+                        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-3 gap-y-3">
+                            {userLends.map((lend) => <NFTCard key={lend.pda.toString()} kind="userLend" data={lend} />)}
+                        </div>
+                    ) : <p className="font-medium text-white/60">You don't have any NFTs you lended for.</p>}
+                </div>
+
             </div>
         </Main>
     )
